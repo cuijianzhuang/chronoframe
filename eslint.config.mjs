fac94e01538f8733014fb3012e7adb15878de255
Timothy Yin <master@uniiem.com>
2025-08-22 01:54:57 +0800

    chore: add eslint, prettier and env example

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,18 @@
 // @ts-check
+import prettier from 'prettier'
 import withNuxt from './.nuxt/eslint.config.mjs'
 
-export default withNuxt(
-  // Your custom configs here
-)
+export default withNuxt({
+  rules: {
+    'vue/html-self-closing': 'off',
+    'vue/no-v-html': 'off',
+    'vue/singleline-html-element-content-newline': 'off',
+    '@stylistic/brace-style': 'off',
+    '@stylistic/arrow-parens': 'off',
+    '@stylistic/operator-linebreak': 'off',
+    '@typescript-eslint/no-empty-object-type': 'off',
+  },
+  plugins: {
+    prettier,
+  },
+})
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -9,5 +9,20 @@ export default defineNuxtConfig({
     '@nuxt/icon',
     '@nuxt/image',
     '@nuxt/test-utils'
-  ]
+  ],
+
+  runtimeConfig: {
+    S3_ENDPOINT: '',
+    S3_BUCKET: '',
+    S3_REGION: '',
+    S3_ACCESS_KEY_ID: '',
+    S3_SECRET_ACCESS_KEY: '',
+    S3_PREFIX: '',
+    S3_CDN_URL: '',
+  },
+
+  nitro: {},
+
+
+  vite: {},
 })
