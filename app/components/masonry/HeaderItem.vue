10ec8cf7b8751df6c2cc48d379da000edb64614d
HoshinoSuzumi <master@uniiem.com>
2025-08-28 03:40:01 +0800

    feat: 为 HeaderItem 添加模糊背景图

--- app/components/masonry/HeaderItem.vue
+++ app/components/masonry/HeaderItem.vue
@@ -19,8 +19,19 @@ const handleOpenLogin = () => {
 </script>
 
 <template>
-  <div class="inline-block w-full align-top break-inside-avoid mb-4">
-    <div class="flex flex-col items-center p-4 pb-2 gap-2">
+  <div
+    class="inline-block w-full align-top break-inside-avoid mb-[4px] relative overflow-hidden"
+  >
+    <div
+      class="absolute inset-0 -z-10 blur-3xl scale-110"
+      :style="{
+        backgroundImage: `url(${avatarImage})`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+      }"
+    ></div>
+    <div class="absolute inset-0 -z-10 bg-neutral-900/50"></div>
+    <div class="flex flex-col items-center py-6 gap-2">
       <AuthState>
         <template #default="{ loggedIn, user, clear }">
           <div class="flex flex-col items-center gap-2">
@@ -39,21 +50,27 @@ const handleOpenLogin = () => {
                 @click="!loggedIn && handleOpenLogin()"
               />
             </div>
-            <h1 class="text-2xl font-bold text-gray-900 dark:text-white mb-2">
+            <h1
+              class="text-2xl font-bold text-neutral-900 dark:text-white/90 mb-2"
+            >
               TimoYin's mems
             </h1>
           </div>
-          <div class="text-gray-600 dark:text-gray-400 space-y-1 text-center">
+          <div
+            class="text-neutral-600 dark:text-white/30 space-y-1 text-center"
+          >
             <p class="text-xs font-medium">
               <span v-if="dateRangeText">{{ dateRangeText }}, 共</span>
               {{ stats?.total }} 张照片
             </p>
             <p class="font-[Pacifico]">Mems rest within the lens.</p>
           </div>
-          <div class="text-center">
+          <div
+            class="text-center mt-3"
+            v-if="loggedIn"
+          >
             <div class="flex items-center gap-2">
               <UButton
-                v-if="loggedIn"
                 size="sm"
                 color="info"
                 variant="soft"
@@ -63,7 +80,6 @@ const handleOpenLogin = () => {
                 仪表盘
               </UButton>
               <UButton
-                v-if="loggedIn"
                 size="sm"
                 color="error"
                 variant="soft"
