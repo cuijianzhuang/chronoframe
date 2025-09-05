4f26b6283f24f12b94947e73ec9e762250e95421
Timothy Yin <master@uniiem.com>
2025-09-06 02:10:31 +0800

    style: 修复加载动画样式丢失

--- app/pages/[...slug].vue
+++ app/pages/[...slug].vue
@@ -64,36 +64,4 @@ const handleClose = () => {
   />
 </template>
 
-<style scoped>
-.loading-scan-wrapper {
-  display: inline-block;
-  position: relative;
-}
-
-.loading-scan-text {
-  display: inline-block;
-  background: linear-gradient(
-    90deg,
-    var(--ui-text-highlighted) 0%,
-    var(--ui-text-highlighted) 30%,
-    var(--ui-text-muted) 50%,
-    var(--ui-text-highlighted) 70%,
-    var(--ui-text-highlighted) 100%
-  );
-  background-size: 200% 100%;
-  background-position: 200% 0;
-  -webkit-background-clip: text;
-  -webkit-text-fill-color: transparent;
-  background-clip: text;
-  color: transparent;
-  animation: scan-x-text 1.2s linear infinite;
-}
-@keyframes scan-x-text {
-  0% {
-    background-position: 200% 0;
-  }
-  100% {
-    background-position: -200% 0;
-  }
-}
-</style>
+<style scoped></style>
