5f5e7c027e539c0f2dcb6fd0cfa0fd19ab381af2
Timothy Yin <master@uniiem.com>
2025-10-26 16:59:26 +0800

    docs: add Discord community link

--- docs/.vitepress/config.ts
+++ docs/.vitepress/config.ts
@@ -61,6 +61,7 @@ export default defineConfig({
 
     socialLinks: [
       { icon: 'github', link: 'https://github.com/HoshinoSuzumi/chronoframe' },
+      { icon: 'discord', link: 'https://discord.gg/mDZjCwdQGk' },
     ],
 
     editLink: {
